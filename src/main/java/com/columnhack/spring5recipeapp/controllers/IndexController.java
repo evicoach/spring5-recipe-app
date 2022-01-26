7ef2249460ddc65ba9c704043699754a3e28a97f
Evi Owhotemu <evicoach@gmail.com>
2022-01-26 10:28:44 +0100

    Initial Commit

---
+++ src/main/java/com/columnhack/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,7 @@
+package com.columnhack.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+}
---
+++ src/test/java/com/columnhack/spring5recipeapp/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.columnhack.spring5recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
